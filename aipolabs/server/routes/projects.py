6c43f6d02a9e43f1ed98739871db535b387f572e
Fixer <14433035+thisisfixer@users.noreply.github.com>
2025-01-29 21:27:55 +0800

    feat: GET /v1/projects to get public projects data owned by the requester (#13)
    New endpoint:
    - GET /v1/projects: get all projects (and its public data, e.g., agents)
    that the user (identified by http bearer) owns.
    
    New test case:
    - test_get_projects_under_user.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Added an endpoint to retrieve projects owned by a user
    - Enhanced project management functionality with improved type
    specifications
    
    - **Tests**
    	- Added test coverage for new project retrieval functionality
    	- Validated project and agent creation and retrieval processes
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- aipolabs/server/routes/projects.py
+++ aipolabs/server/routes/projects.py
@@ -1,11 +1,11 @@
-from typing import Annotated, Any
+from typing import Annotated
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from aipolabs.common.db import crud
-from aipolabs.common.db.sql_models import Agent, User
+from aipolabs.common.db.sql_models import Agent, Project, User
 from aipolabs.common.enums import OrganizationRole
 from aipolabs.common.logging import get_logger
 from aipolabs.common.schemas.agent import AgentCreate, AgentPublic
@@ -23,7 +23,7 @@ async def create_project(
     body: ProjectCreate,
     user: Annotated[User, Depends(deps.validate_http_bearer)],
     db_session: Annotated[Session, Depends(deps.yield_db_session)],
-) -> Any:
+) -> Project:
     logger.info(f"Creating project={body}, user={user.id}")
     owner_id = body.organization_id or user.id
     # if project is to be created under an organization, check if user has admin access to the organization
@@ -37,6 +37,21 @@ async def create_project(
     db_session.commit()
     logger.info(f"Created project: {project}")
     return project
+
+
+@router.get("/", response_model=list[ProjectPublic], include_in_schema=True)
+async def get_projects(
+    user: Annotated[User, Depends(deps.validate_http_bearer)],
+    db_session: Annotated[Session, Depends(deps.yield_db_session)],
+) -> list[Project]:
+    """
+    Get all projects that the user is the owner of
+    """
+    # TODO: for now, we only support getting projects that the user is the owner of,
+    # we will need to support getting projects that the user has access to (a member of an organization)
+    logger.info(f"Getting projects for user={user.id}")
+    projects = crud.projects.get_projects_by_owner(db_session, user.id)
+    return projects
 
 
 @router.post("/{project_id}/agents/", response_model=AgentPublic, include_in_schema=True)
--- aipolabs/server/tests/routes/projects/test_projects.py
+++ aipolabs/server/tests/routes/projects/test_projects.py
@@ -74,4 +74,44 @@ def test_create_agent(
     assert len(agent_public.api_keys) == 1
     assert agent_public.api_keys[0].key == api_key.key
 
-    # Clean up: no need to delete agent and api key, it will be deleted when dummy_project_1 is deleted
+
+def test_get_projects_under_user(
+    test_client: TestClient, db_session: Session, dummy_user_bearer_token: str, dummy_user: User
+) -> None:
+    # create projects and agents under the user
+    number_of_projects = 3
+    number_of_agents_per_project = 2
+    for i in range(number_of_projects):
+        body = ProjectCreate(name=f"project_{i}")
+        response = test_client.post(
+            f"{config.ROUTER_PREFIX_PROJECTS}/",
+            json=body.model_dump(mode="json"),
+            headers={"Authorization": f"Bearer {dummy_user_bearer_token}"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        project_public = ProjectPublic.model_validate(response.json())
+
+        for j in range(number_of_agents_per_project):
+            body = AgentCreate(
+                name=f"project_{i}_agent_{j}", description=f"project_{i}_agent_{j} description"
+            )
+            response = test_client.post(
+                f"{config.ROUTER_PREFIX_PROJECTS}/{project_public.id}/agents/",
+                json=body.model_dump(mode="json"),
+                headers={"Authorization": f"Bearer {dummy_user_bearer_token}"},
+            )
+            assert response.status_code == status.HTTP_200_OK
+
+    # get projects under the user
+    response = test_client.get(
+        f"{config.ROUTER_PREFIX_PROJECTS}/",
+        headers={"Authorization": f"Bearer {dummy_user_bearer_token}"},
+    )
+    assert response.status_code == status.HTTP_200_OK
+    projects_public = [ProjectPublic.model_validate(project) for project in response.json()]
+    assert len(projects_public) == number_of_projects
+    for project in projects_public:
+        agents_public = [AgentPublic.model_validate(agent) for agent in project.agents]
+        assert len(agents_public) == number_of_agents_per_project
+        for agent in agents_public:
+            assert len(agent.api_keys) == 1, "each agent should have one api key"
