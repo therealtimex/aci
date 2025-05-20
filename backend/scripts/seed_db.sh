c4d8482d6cad01297deb1b4e918aef8ebf3c794c
Fixer <14433035+thisisfixer@users.noreply.github.com>
2025-05-20 18:26:58 +0100

    Update seed_db.sh

--- backend/scripts/seed_db.sh
+++ backend/scripts/seed_db.sh
@@ -15,8 +15,7 @@ Usage: $0 [options]
 Options:
   -h, --help   Display this help message
   -a, --all    Seed all available Apps and Functions (without this flag, it will seed only a selected set of Apps and their Functions (with dummy OAuth2 client id & secret if it's OAuth2 app))
-  -m, --mock   Create .app.secrets.json files with mock credentials for OAuth2 apps
-
+  -m, --mock   Use mock oauth2 client id & client secret for OAuth2 apps (otherwise you'll need to provide them in .app.secret.json for each app)
 EOF
 }
 
